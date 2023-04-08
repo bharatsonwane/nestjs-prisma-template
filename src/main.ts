f97779633ace5d6b5a1fd5979515a2ad3e5b8adb
Bharat Sonwane <bharatrsonwane@gmail.com>
2023-04-08 22:17:16 +0530

    port update

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,6 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();
